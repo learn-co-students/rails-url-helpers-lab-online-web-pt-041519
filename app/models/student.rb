d0b8033cd1fd787e9dc3ed3cbb99ba69b13df6ad
github username <github email address>
2019-09-08 00:06:46 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,12 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def active_message
+    self.active ? "This student is currently active." : "This student is currently inactive."
+  end
+
+  def toggle_active
+    self.active == true ? self.update(active: false) : self.update(active: true)
+  end
 end
